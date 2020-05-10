f05f248c7de4fc6bbcfcb22ea47bf3f72f0d7a5e
Dinithi <dinithimb@gmail.com>
2020-05-10 22:51:55 +0530

    Added Directories 3

---
+++ src/Components/AddNew/AddDiscount/AddDiscount.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class AddDiscount extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default AddDiscount;
